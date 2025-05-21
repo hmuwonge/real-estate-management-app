a67666dd2708c9d32e01831ad1a71685665c5013
MUWONGE HASSAN <hassansaava@gmail.com>
2025-05-21 10:09:34 +0300

    fix db context

---
+++ aspnet-core/src/SawaTech.PropertyMini.Domain/Users/AccountUser.cs
@@ -0,0 +1,22 @@
+﻿using Volo.Abp.Identity;
+
+namespace SawaTech.PropertyMini.Users;
+
+public class User : IdentityUser
+{
+    public UserType Type { get; set; }
+    public string? CompanyName { get; set; }
+    public string? CompanyEmail { get; set; }
+    public string? Department { get; set; }
+    public string? JobPosition { get; set; }
+    public string? Phone { get; set; }
+    public string? WhatsApp { get; set; }
+    public string? Country { get; set; }
+    public string? ProfilePictureUrl { get; set; }
+}
+
+
+public enum UserType
+{
+    Agent,Visitor
+}
