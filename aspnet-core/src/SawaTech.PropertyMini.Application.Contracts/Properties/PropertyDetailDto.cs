50d481fa244395cc40a7b9841b134ef791d14bb2
MUWONGE HASSAN <hassansaava@gmail.com>
2025-06-01 12:22:57 +0300

    update: resolved memebts issue in propertydetailsdto

--- aspnet-core/src/SawaTech.PropertyMini.Application.Contracts/Properties/PropertyDetailDto.cs
+++ aspnet-core/src/SawaTech.PropertyMini.Application.Contracts/Properties/PropertyDetailDto.cs
@@ -8,15 +8,15 @@ namespace SawaTech.PropertyMini.Properties
 {
     public class PropertyDetailDto : PropertyListDto
     {
-        public string Description { get; set; } = string.Empty;
+        public new string Description { get; set; } = string.Empty;
         public string Address { get; set; }= string.Empty;
-        public decimal InsurancePayment { get; set; }
+        public new decimal InsurancePayment { get; set; }
         public string Status { get; set; }= string.Empty;
         public new PropertyTypeDto PropertyType { get; set; }= null!;
         public string PaymentType { get; set; }= string.Empty;
-        public new List<PropertyImageDto> Images { get; set; }= null!;
+        public List<PropertyImageDto> Images { get; set; }= null!;
         public new List<PropertyFeatureDto> Features { get; set; }= null!;
         public new List<SinglePropertyAmenityDto> Amenities { get; set; } = null!;
-        public new List<NearbyPlaceDto> NearbyPlaces { get; set; }= null!;
+        public List<NearbyPlaceDto> NearbyPlaces { get; set; }= null!;
     }
 }
