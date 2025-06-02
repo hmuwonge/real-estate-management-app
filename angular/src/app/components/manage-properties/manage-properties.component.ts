e3610e1591bafca4580a61e7934717d8122ce0cc
MUWONGE HASSAN <hassansaava@gmail.com>
2025-06-02 19:37:09 +0300

    fix: resolved main git issues

--- angular/src/app/components/manage-properties/manage-properties.component.ts
+++ angular/src/app/components/manage-properties/manage-properties.component.ts
@@ -16,6 +16,9 @@ import { UsersService } from '../../services/users/users.service';
 export class ManagePropertiesComponent implements OnInit {
   userId: string | null;
   properties: SimilarProperty[] | [] = []
+  isLoading: boolean = true;
+  isError: boolean = false;
+  defaultImage: string = '/images/placeholder.jpg';
 
   constructor(private propertiesServices: PropertiesService, private userService: UsersService) {
     this.userId = localStorage.getItem('userId');
@@ -26,11 +29,14 @@ export class ManagePropertiesComponent implements OnInit {
     console.log(user)
 
     if (user) {
+      this.isLoading = true;
+      this.isError = false;
       this.propertiesServices.getUserProperties(user.id).pipe(handleResponse<SimilarProperty[]>())
         .subscribe({
           next: (response: SimilarProperty[]) => {
             if (response) {
               this.properties = response
+              this.isLoading = false;
             }
           },
           error: (err) => {
